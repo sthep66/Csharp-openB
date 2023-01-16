acae0d6739b8210178d2c3e7d4b70f4ef8bd44c5
steph peves <82428583+sthep66@users.noreply.github.com>
2023-01-16 13:33:25 -0300

    add exercises session 1

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿// ***  EJERCICIO 1
+
+Console.WriteLine("Escribe tu nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine($"Hola {nombre}");
+
+
+// ***  EJERCICIO 2
+
+string time = DateTime.Now.ToString("t");
+
+Console.WriteLine($"La hora es {time}");

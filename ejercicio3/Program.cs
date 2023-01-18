7f16e0ce86b61fbf626c4933a244f216bc136691
steph peves <82428583+sthep66@users.noreply.github.com>
2023-01-18 12:37:15 -0300

    add exercises session 3

---
+++ ejercicio3/Program.cs
@@ -0,0 +1,26 @@
+﻿// *** EJERCICIO 1
+
+Clientes cli = new Clientes("Lola Bunny", 12356, "Looney Tunes 123", "lolab@gmail.com", true);
+Console.WriteLine(cli);
+
+public struct Clientes {
+    public Clientes( string nom, int telf, string dir, string mail, bool nuevo){
+        nombre = nom;
+        telefono = telf;
+        direccion = dir;
+        email = mail;
+        nuevo_cliente = nuevo;
+    }
+
+    public string nombre {get; set;}
+    public int telefono {get; set;}
+    public string direccion {get; set;}
+    public string email {get; set;}
+    public bool nuevo_cliente {get; set;}
+
+    public override string ToString()
+    {
+        return $"Nombre completo: {nombre}\n Teléfono: {telefono}\n Dirección: {direccion}\n Email: {email}\n Cliente nuevo: {nuevo_cliente}";
+    }
+}
+
